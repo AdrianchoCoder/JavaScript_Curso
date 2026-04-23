335d7efa5323625e569fe1cc3caafa45a8b6b60d
AdrianchoCoder <adriandaguirrej@gmail.com>
2026-04-22 21:34:14 -0500

    Primer Estudie de JavaScript 22/04/2026
    Se Estudio Todo lo Relacionado a Variables y a Operadores que se utilizan en JavaScript.

---
+++ Curso 1/1.1-variablesJavaScript.js
@@ -0,0 +1,40 @@
+// Tipos de Datos En JavaScript
+// Tipo Number - Entero
+let miEntero = 10;
+console.log(miEntero);
+
+// Tipo Float - Flotante o Decimal
+let miFlotante = 10.5;
+console.log(miFlotante);
+
+// Tipo String - Cadena o Texto
+let miString = 'Hola Mundo';
+console.log(miString);
+
+// Tipo Booleano - Verdadero o Falso
+let miBooleano = true;
+console.log(miBooleano);
+
+// TIpo Null - Vacio
+let miNull = null;
+console.log(miNull);
+
+// Tipo Undefined - Indefinido
+let miUndefined = undefined;
+console.log(miUndefined);
+
+
+// Para Conocer que Tipo de Dato tenemos en Nuestra Variable Si es que Ocurre algun Error, etc. Utilizamos " TypeOf "  Para Saber el Tipo de Dato.
+console.log(typeof miEntero);
+console.log(typeof miBooleano);
+console.log(typeof miFlotante);
+console.log(typeof miString);
+console.log(typeof miNull);
+console.log(typeof miUndefined);
+
+
+// Constantes --> Una Constante es un Valor que no Puede Ser Modificado cuando se Coloca el Nombre de la Constante debe ser Siempre en Mayuscula y Si tiene mas de Una Palabra se Coloca Guion Bajo (_).
+const PI = 3.14;
+const MI_CONSTANTE = 'Nombre Clave: Adrian';
+console.log(PI);
+console.log(MI_CONSTANTE);
---
+++ Curso 1/1.2-operadoresJavaScript.js
@@ -0,0 +1,32 @@
+// Operadores Aritmeticos --> Los Operadores que Utiliza JavaScript son los Siguentes:
+// Suma:
+let numeroSumaA = 10;
+let numeroSumaB = 20;
+let resultado1 = numeroSumaA + numeroSumaB;
+console.log(resultado1);
+
+// Resta:
+let numeroRestaA = 10;
+let numeroRestaB = 5;
+let resultado2 = numeroRestaA - numeroRestaB;
+console.log(resultado2);
+
+// Multiplicacion:
+let numeroMultiplicacionA = 3;
+let numeroMultiplicacionB = 5;
+let resultado3 = numeroMultiplicacionA * numeroMultiplicacionB;
+console.log(resultado3);
+
+// Division:
+let numeroDivisionA = 10;
+let numeroDivisionB = 2;
+let resultado4 = numeroDivisionA / numeroDivisionB;
+console.log(resultado4);
+
+// Modulo (Residuo Division):
+let resultado5 = 10 % 2;
+console.log(resultado5);
+
+// Potencia:
+let resultado6 = 10 ** 2;
+console.log(resultado6);
