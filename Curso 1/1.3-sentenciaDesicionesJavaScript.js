a31d59dabf73e6efc817fb3de0533b5ae68462d7
AdrianchoCoder <adriandaguirrej@gmail.com>
2026-04-29 14:24:44 -0500

    Sentencia Desiciones JavaScript

--- Curso 1/1.3-sentenciaDesicionesJavaScript.js
+++ Curso 1/1.3-sentenciaDesicionesJavaScript.js
@@ -25,3 +25,46 @@ else{
     console.log('Verifica que tu Numero sea un Numero!');
 }
 // Como se Puede Observar Esto es un buen uso de ' if - else if - else ' ya que se agregan mas condiciones para que no hayan errores en nuestro codigo.
+
+// Operador Ternario --> Es una simplificacion del if else
+(miNumero >= 0) ? console.log('Positivo') : console.log('Negativo');
+
+// Ejemplo Mayor de Edad Usuario
+const MAYOR_EDAD = 18;
+let user_edad = 0;
+if (user_edad >= MAYOR_EDAD){
+    console.log(`El usuario con edad: ${user_edad} es mayor de edad`);
+}
+else if (user_edad < 18 && user_edad > 0){
+    console.log(`El Usuario de edad: ${user_edad} es menor de edad`);
+}
+else{
+    console.log(`Lo siento, revisa que el dato ingresado sea numerico o Positivo.`);
+}
+
+// Ejemplo Dia de la Semana
+let diaSemana = 'Sabado';
+if (diaSemana == 'Lunes'){
+    console.log(`El dia de hoy es: ${diaSemana}`);
+}
+else if (diaSemana == 'Martes'){
+    console.log(`El dia de hoy es: ${diaSemana}`);
+}
+else if (diaSemana == 'Miercoles'){
+    console.log(`El dia de hoy es: ${diaSemana}`);
+}
+else if (diaSemana == 'Jueves'){
+    console.log(`El dia de hoy es: ${diaSemana}`);
+}
+else if (diaSemana == 'Viernes'){
+    console.log(`El dia de hoy es: ${diaSemana}`);
+}
+else if (diaSemana == 'Sabado'){
+    console.log(`El dia de hoy es: ${diaSemana}`);
+}
+else if (diaSemana == 'Domingo'){
+    console.log(`El dia de hoy es: ${diaSemana}`);
+}
+else{
+    console.log('Verifica que sea uno de los dias de la semana');
+}
