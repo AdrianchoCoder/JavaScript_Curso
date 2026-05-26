c2bde16ac3cb6f082016202b8fa83610ab54e414
AdrianchoCoder <adriandaguirrej@gmail.com>
2026-05-25 23:34:14 -0500

    Cree el Archivo de Matrices en JavaScript

---
+++ Curso 1/1.6-matricesEnJavaScript.js
@@ -0,0 +1,16 @@
+// Matrices en JavaScript
+// Declaracion de una Matriz Manualmente
+let matriz = [[],[]];
+// Modificar los Valores de una Matriz
+// Fila 0
+matriz[0][0] = 100;
+matriz[0][1] = 200;
+// Fila 1
+matriz[1][0] = 300;
+matriz[1][1] = 400;
+console.log(matriz);
+
+// Declaracion de una Matriz mas Sencillo
+let matriz2 = [[100, 200, 300, 500],[400, 500, 600]];
+console.log(`Elemento [0][2]: ${matriz2[0][2]}`);
+console.log(`Elemento [1][0]: ${matriz2[1][0]}`);
